f224b3f939e382406d9991f24c92ec1bacf62491
HadiAnik <hadiuzzamananik@gmail.com>
2023-04-07 21:35:15 +0600

    implement only  Top 20 Result show

--- src/Component/Student/LeaderBoard/AllStudentPositionList.jsx
+++ src/Component/Student/LeaderBoard/AllStudentPositionList.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const AllStudentPositionList = ({ data }) => {
   const totalMarkArray = data.map((std) => std.totalMark);
-  const uniqueTotalMarkArray = [...new Set(totalMarkArray)].slice();
+  const uniqueTotalMarkArray = [...new Set(totalMarkArray)].slice(0, 20);
+  // Top 20 last value index in unique total mark
+  const lastTotalMarkNumber = uniqueTotalMarkArray.slice(-1)[0];
+
   return (
     data.length > 0 && (
       <div className="my-8">
@@ -20,19 +23,24 @@ const AllStudentPositionList = ({ data }) => {
 
           <tbody>
             {data?.map((stu, i) => {
-              const position =
-                uniqueTotalMarkArray.findIndex(
-                  (item) => item === stu.totalMark
-                ) + 1;
-              return (
-                <tr className="border-b border-slate-600/50" key={i}>
-                  <td className="table-td text-center">{position}</td>
-                  <td className="table-td text-center">{stu.name}</td>
-                  <td className="table-td text-center">{stu.quizMark}</td>
-                  <td className="table-td text-center">{stu.assignmentMark}</td>
-                  <td className="table-td text-center">{stu.totalMark}</td>
-                </tr>
-              );
+              if (lastTotalMarkNumber <= stu.totalMark) {
+                const position =
+                  uniqueTotalMarkArray.findIndex(
+                    (item) => item === stu.totalMark
+                  ) + 1;
+                return (
+                  <tr className="border-b border-slate-600/50" key={i}>
+                    <td className="table-td text-center">{position}</td>
+                    <td className="table-td text-center">{stu.name}</td>
+                    <td className="table-td text-center">{stu.quizMark}</td>
+                    <td className="table-td text-center">
+                      {stu.assignmentMark}
+                    </td>
+                    <td className="table-td text-center">{stu.totalMark}</td>
+                  </tr>
+                );
+              }
+              return null;
             })}
           </tbody>
         </table>
